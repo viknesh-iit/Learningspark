b51334679e02526c50cae108e8f2f7f9f3841cb9
Viknesh Koodalingam <viknesh.iit@gmail.com>
2016-12-11 18:37:20 +0530

    Test

--- sparkstreaming/src/main/scala/com/data/slyinsight/sparkstreaming/print.scala
+++ sparkstreaming/src/main/scala/com/data/slyinsight/sparkstreaming/print.scala
@@ -4,18 +4,9 @@ import scala.App
 
 class print {
   
-<<<<<<< HEAD
   def main extends App(args: Array[String]) {
   print("Sooper")
   }
   
   
 }
-=======
-  def main(args: Array[String]) {
-    print("Sooper")
-  }
-  
-  
-}
->>>>>>> branch 'master' of https://github.com/viknesh-iit/Learningspark.git
